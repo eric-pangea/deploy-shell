4ee83d088a07cc3c752d18a38092f93acd599329
eric-pangea <98314134+eric-pangea@users.noreply.github.com>
2023-01-17 17:16:42 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 for REPO in $@; do
-	curl \
-	  -X POST \
-	  -H "Accept: application/vnd.github+json" \
-	  -H "Authorization: Bearer $MY_GIT_PAT \
-	  -H "X-GitHub-Api-Version: 2022-11-28" \
-	  https://api.github.com/repos/eric-pangea/testrepo2/dispatches \
-	  -d '{"event_type":"test-event"}'
+	echo "dispatching $REPO" >> test.txt
+# 	curl \
+# 	  -X POST \
+# 	  -H "Accept: application/vnd.github+json" \
+# 	  -H "Authorization: Bearer $MY_GIT_PAT \
+# 	  -H "X-GitHub-Api-Version: 2022-11-28" \
+# 	  https://api.github.com/repos/eric-pangea/testrepo2/dispatches \
+# 	  -d '{"event_type":"test-event"}'
 done
